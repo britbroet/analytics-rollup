64cbae3814c8382e6a47916514fcc8f3e388d01c
britbroet <brittanybroetje@gmail.com>
2017-01-13 07:36:06 -0800

    stormpath auth added

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var bodyParser = require("body-parser"); //read post variables
 var fs = require("fs"); //read from & write to files
 var ejsLayouts = require("express-ejs-layouts"); 
 var request = require("request"); //XMLHttp Requests (e.g., AJAX)
-//require('dotenv').config();
+var stormpath = require('express-stormpath');
+require('dotenv').config();
 //var flash = require('connect-flash');
 var app = express();
+
+app.use(stormpath.init(app, {
+  website: true
+}));
 
 app.set('view engine', 'ejs');
 
@@ -20,6 +25,10 @@ app.get('/', function(req, res) {
 });
 
 
-var server = app.listen(process.env.PORT || 3000);
+app.on('stormpath.ready', function() {
+  server = app.listen(process.env.PORT || 3000);
+  module.exports = server;
+});
 
-module.exports = server;
+
+
